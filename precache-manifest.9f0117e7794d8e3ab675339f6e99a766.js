bbe70cc259ab01d75fce4d2523cad1376ce00299
ravitheja44 <ravitheja544@gmail.com>
2020-07-27 15:03:56 +0530

    Updates

---
+++ precache-manifest.9f0117e7794d8e3ab675339f6e99a766.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbe262930d551011287911f36286a43",
+    "url": "/git-my-app/index.html"
+  },
+  {
+    "revision": "c1ca9bfa46a645894d4d",
+    "url": "/git-my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84d8e7a3a62c95b887dc",
+    "url": "/git-my-app/static/js/2.647a045e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-my-app/static/js/2.647a045e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ca9bfa46a645894d4d",
+    "url": "/git-my-app/static/js/main.37d2bff5.chunk.js"
+  },
+  {
+    "revision": "42108ab9afa352992879",
+    "url": "/git-my-app/static/js/runtime-main.afef8204.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
